a6cb29dbe8e496cbcb61ef5be7387d2d9d0d5080
Asikul Islam Sazzat <asikulislamsazzat@gmail.com>
2024-12-04 21:34:02 +0600

    Registration Verify

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,1 +1,43 @@
-//
+@extends('frontend.layouts.app')
+@section('title')
+    Account Verify
+@endsection
+@push('style')
+@endpush
+@section('content')
+    @include('frontend.layouts.include.banner', [
+        'page_name' => 'Account Verify',
+        'subpage_name' => '',
+    ])
+    <section id="login_arae" class="section_padding">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 offset-lg-3 col-md-12 col-sm-12 col-12">
+                    <div class="section_heading">
+                        <p>Thanks for signing up! Before getting started, could you verify your email address by clicking on
+                            the link we just emailed to you? If you didn't receive the email, we will gladly send you
+                            another.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-6 offset-lg-3">
+                    <div class="author_form_area">
+                        @if (session('status') == 'verification-link-sent')
+                            <div class="text-center text-success text-bold">
+                                {{ __('A new verification link has been sent to the email address you provided during registration.') }}
+                            </div>
+                        @endif
+                        <form method="POST" action="{{ route('verification.send') }}">
+                            @csrf<div class="author_submit_form">
+                                <button class="btn btn_theme btn_md">Resend Verification Email</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
+@push('script')
+@endpush
